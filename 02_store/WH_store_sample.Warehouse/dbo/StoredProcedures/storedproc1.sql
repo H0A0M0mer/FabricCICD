ab95ae3d5e4db549bc8a0daacc514612dfcafd09
H0A0M0mer <72689045+H0A0M0mer@users.noreply.github.com>
2025-06-12 09:34:56 +0200

    Committing 2 items from workspace 3a4b7256-9761-4633-ae8e-a679bc1b4264

--- 02_store/WH_store_sample.Warehouse/dbo/StoredProcedures/storedproc1.sql
+++
@@ -1,7 +0,0 @@
-CREATE PROC [dbo].[storedproc1]
-@param1 int,
-@param2 int
-AS
-BEGIN
-SELECT @param1, @param2
-END
--- 02_store/WH_store_sample.Warehouse/dbo/StoredProcedures/storedproc2.sql
+++
@@ -1,7 +0,0 @@
-CREATE PROC [dbo].[storedproc2]
-@param1 int,
-@param2 int
-AS
-BEGIN
-SELECT @param1, @param2
-END
